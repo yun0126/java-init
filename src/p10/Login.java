5770bbe2b19cf31d016c237711e4d83ccb036f5f
qudals <qudals159753@naver.com>
2023-07-07 18:11:28 +0900

    Merge branch 'master' of https://github.com/yun0126/java-init.git

--- src/p10/Login.java
+++ src/p10/Login.java
@@ -1,6 +1,7 @@
 package p10;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,9 +20,17 @@ public class Login {
 		String sql = "SELECT * FROM user_info ";
 		sql += " WHERE UI_ID = '" + uiId + "' AND UI_PWD='" + uiPwd + "'";
 
-		Connection con = DBCon.getCon();
+		String sql1 = "INSERT INTO user_info(UI_ID, UI_PWD, UI_NAME)\r\n" + "VALUES('TEST00', 'TEST00', '연습00')";
+
+		String sql2 = "UPDATE USER_INFO SET UI_NAME = '동동이' WHERE UI_NUM=6";
+
+		Connection con = DBCon.getCon();  
 		try {
 			Statement stmt = con.createStatement();
+			int signIn = stmt.executeUpdate(sql1);
+			System.out.println("회원가입 된 계정" + signIn);
+			int update = stmt.executeUpdate(sql2);
+			System.out.println("계정 정보 수정");
 			ResultSet rs = stmt.executeQuery(sql);
 			if (rs.next()) {
 				String uiName = rs.getString("UI_NAME");
